81c359da3a6d9427c05fcfc3235e4886d0806e17
Dylan Burton <37320555+dylanburton@users.noreply.github.com>
2023-12-18 04:26:23 -0600

    Got The application to create trainers for playthroughs and am now working on getting teams setup.

---
+++ NuzlockeAssistant/Models/Pokemon.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace NuzlockeAssistant.Models
+{
+    public class Pokemon
+    {
+        [Key]
+        public int Number { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [AllowNull]
+        public string Nickname { get; set; }
+        public string Type1 { get; set; }
+        [AllowNull]
+        public string Type2 { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Special { get; set; }
+        public int Speed { get; set; }
+        [AllowNull]
+        public string EncounterRoute { get; set; } = string.Empty;
+        public bool Captured { get; set; } = false;
+        public bool Alive { get; set; } = true;
+
+
+        /*public Pokemon(string number, string name, string type1, string type2, int hP, int attack, int defense, int special, int speed, string location = "Starter")
+        {
+            Number = number;
+            Name = name;
+            Nickname = name;
+            Type1 = type1;
+            Type2 = type2;
+            HP = hP;
+            Attack = attack;
+            Defense = defense;
+            Special = special;
+            Speed = speed;
+
+        }*/
+    }
+}
---
+++ NuzlockeAssistant/Models/Trainer.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace NuzlockeAssistant.Models
+{
+
+    public class Trainer
+    {
+        public int ID { get; set; }
+        [AllowNull]
+        public string Name { get; set; }
+        [AllowNull]
+        public List<Pokemon> Team { get; set; }
+        [AllowNull]
+        public List<Pokemon> Box { get; set; }
+        [AllowNull]
+        /*used mainly for encounter stats, moveset, etc.*/
+        public Pokemon Encounter { get; set; }
+        [AllowNull]
+        public Pokemon DeathBox { get; set; }
+        /*used to track major battles for level cap
+         * TODO: Fill in when opportunity arrises.
+         */
+        /*public bool[] Progress  { get; set; }*/
+        public int LevelCap { get; set; }
+        public bool Alive { get; set; } = true;
+
+
+    }
+
+
+}
+
